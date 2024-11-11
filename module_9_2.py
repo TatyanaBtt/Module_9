f6324fa649eaaf1fc9ec6cdf9d8260eb010f7c9e
TatyanaBtt <2269821@gmail.com>
2024-11-11 20:47:41 +0300

    add

---
+++ module_9_2.py
@@ -0,0 +1,13 @@
+from gc import is_finalized
+
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+combined_strings = first_strings + second_strings
+
+first_result = [len(x) for x in first_strings if len(x) >= 5]
+second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
+third_result = {x: len(x) for x in combined_strings if len(x)%2==0}
+
+print(first_result)
+print(second_result)
+print(third_result)
