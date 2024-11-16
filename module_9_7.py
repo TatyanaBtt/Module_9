42dd8a99306116886c76b19fe06212bc2e7834d2
TatyanaBtt <2269821@gmail.com>
2024-11-16 21:36:39 +0300

    add

---
+++ module_9_7.py
@@ -0,0 +1,26 @@
+def is_prime(func):
+    def wrapper(*args):
+        result = func(*args)
+        prime = True
+        for j in range(2, result):
+            if result % j == 0:
+                prime = False
+            break
+        if prime:
+            print('Простое')
+            return result
+        else:
+            print('Составное')
+            return result
+
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    rez = a + b + c
+    return rez
+
+
+result = sum_three(11, 12, 5)
+print(result)
